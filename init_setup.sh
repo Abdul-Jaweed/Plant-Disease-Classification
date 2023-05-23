d6b166ccbb0ba1f59dd59cbf3f355aad3d4b0264
Abdul Jaweed <jdgaming7320@gmail.com>
2023-05-24 00:36:27 +0530

    conda enviroment created and activated

---
+++ init_setup.sh
@@ -0,0 +1,6 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "activate env"
+source activate ./env
+echo [$(date)]: "END"
